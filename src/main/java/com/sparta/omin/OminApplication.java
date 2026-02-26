95c678c3a0c054666fd653c57160b58e257dea25
sunsik17 <17sunsik@gmail.com>
2026-02-26 14:55:49 +0900

    initial commit

---
+++ src/main/java/com/sparta/omin/OminApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.omin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OminApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/omin/OminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.omin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
