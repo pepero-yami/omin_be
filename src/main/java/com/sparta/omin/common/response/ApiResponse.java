48d1d6b01701d6f28bb883325211c743d918183d
ji-circle <sp202401jw@gmail.com>
2026-03-01 15:25:46 +0900

    fix: 사용하지 않는 코드 삭제 - ApiResponse.java, ResponseUtil.java

--- src/main/java/com/sparta/omin/common/response/ApiResponse.java
+++ src/main/java/com/sparta/omin/common/response/ApiResponse.java
@@ -1,24 +1,24 @@
-package com.sparta.omin.common.response;
-
-import com.fasterxml.jackson.annotation.JsonInclude;
-import lombok.Getter;
-
-@Getter
-@JsonInclude(JsonInclude.Include.NON_NULL)
-public class ApiResponse<T> {
-
-    private final int status;
-    private final String message;
-    private final T data;
-
-    private ApiResponse(int status, String message, T data) {
-        this.status = status;
-        this.message = message;
-        this.data = data;
-    }
-
-    public static <T> ApiResponse<T> of(int status, String message, T data) {
-        return new ApiResponse<>(status, message, data);
-    }
-
-}
+//package com.sparta.omin.common.response;
+//
+//import com.fasterxml.jackson.annotation.JsonInclude;
+//import lombok.Getter;
+//
+//@Getter
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+//public class ApiResponse<T> {
+//
+//    private final int status;
+//    private final String message;
+//    private final T data;
+//
+//    private ApiResponse(int status, String message, T data) {
+//        this.status = status;
+//        this.message = message;
+//        this.data = data;
+//    }
+//
+//    public static <T> ApiResponse<T> of(int status, String message, T data) {
+//        return new ApiResponse<>(status, message, data);
+//    }
+//
+//}
