8582dfd87d33fda37d6f004ceef0ac8463b5302b
Yoonho Cha <0119hoya@naver.com>
2026-03-03 00:19:04 +0900

    feat: Cart Controller작성

--- src/main/java/com/sparta/omin/app/controller/cart/CartController.java
+++ src/main/java/com/sparta/omin/app/controller/cart/CartController.java
@@ -3,14 +3,16 @@ package com.sparta.omin.app.controller.cart;
 import com.sparta.omin.app.model.cart.dto.CartCreateRequest;
 import com.sparta.omin.app.model.cart.dto.CartResponse;
 import com.sparta.omin.app.model.cart.service.CartService;
+import com.sparta.omin.app.model.cartItem.dto.CartItemResponse;
+import com.sparta.omin.app.model.cartItem.dto.CartItemUpdateRequest;
+import com.sparta.omin.app.model.cartItem.service.CartItemService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
@@ -18,9 +20,34 @@ import org.springframework.web.bind.annotation.RestController;
 public class CartController {
 
     private final CartService cartService;
+    private final CartItemService cartItemService;
 
+    /**
+     * 첫 상품 담기 → 카트 없으면 카트 생성 + 카트 아이템 추가
+     * 두 번째 상품 담기 → 기존 카트에 카트 아이템만 추가
+     * 주문 완료 → 카트 삭제 (or 비우기)
+     */
     @PostMapping("/cart")
     public ResponseEntity<CartResponse> create(@Valid @RequestBody CartCreateRequest request) {
         return ResponseEntity.status(HttpStatus.CREATED).body(cartService.create(request));
+    }
+
+    /**
+     * TODO 인증 구현시 변경
+     */
+    @GetMapping("/cart/{userId}")
+    public ResponseEntity<CartResponse> get(@PathVariable UUID userId) {
+        return ResponseEntity.ok(cartService.get(userId));
+    }
+
+    // controller 분리 고려
+    @PostMapping("/cart/{cartId}")
+    public ResponseEntity<CartItemResponse> addCartItem(@PathVariable UUID cartId, @RequestBody CartItemUpdateRequest request) {
+        return ResponseEntity.ok(cartItemService.create(cartId, request));
+    }
+
+    @PatchMapping("/cart/{cartId}")
+    public ResponseEntity<CartItemResponse> updateCartItem(@PathVariable UUID cartId, @RequestBody CartItemUpdateRequest request) {
+        return ResponseEntity.ok(cartItemService.update(cartId, request));
     }
 }
