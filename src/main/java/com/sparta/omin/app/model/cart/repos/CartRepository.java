77f6081d34b02e0e6d91e18327ecec59c0972a01
Yoonho Cha <0119hoya@naver.com>
2026-03-03 20:32:41 +0900

    fix: 조회를 위한 조인전략 변경(inner join -> left join)

--- src/main/java/com/sparta/omin/app/model/cart/repos/CartRepository.java
+++ src/main/java/com/sparta/omin/app/model/cart/repos/CartRepository.java
@@ -10,7 +10,7 @@ import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, UUID> {
     Optional<Cart> findAllByIsDeletedFalse();
-    @Query("SELECT c FROM Cart c JOIN FETCH c.cartItems WHERE c.userId = :userId AND c.isDeleted = false")
+    @Query("SELECT c FROM Cart c LEFT JOIN FETCH c.cartItems ci WHERE c.userId = :userId AND c.isDeleted = false")
     Optional<Cart> findByUserIdAndIsDeletedFalseWithItems(@Param("userId") UUID userId);
     Optional<Cart> findByUserIdAndIsDeletedFalse(UUID uuid);
 }
