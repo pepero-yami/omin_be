c9de36c2b2752ef6146684e3ea91a022d5cccd8d
Yoonho Cha <0119hoya@naver.com>
2026-03-03 12:16:12 +0900

    fix: Cart, CartItem Repository 작성

--- src/main/java/com/sparta/omin/app/model/cart/repos/CartRepository.java
+++ src/main/java/com/sparta/omin/app/model/cart/repos/CartRepository.java
@@ -2,8 +2,15 @@ package com.sparta.omin.app.model.cart.repos;
 
 import com.sparta.omin.app.model.cart.entity.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface CartRepository extends JpaRepository<Cart, UUID> {
+    Optional<Cart> findAllByIsDeletedFalse();
+    @Query("SELECT c FROM Cart c JOIN FETCH c.cartItems WHERE c.userId = :userId AND c.isDeleted = false")
+    Optional<Cart> findByUserIdAndIsDeletedFalseWithItems(@Param("userId") UUID userId);
+    Optional<Cart> findByUserIdAndIsDeletedFalse(UUID uuid);
 }
