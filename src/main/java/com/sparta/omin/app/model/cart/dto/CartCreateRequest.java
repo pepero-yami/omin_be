534e41bfff56e98246cf373cbbe3f9833ab445b7
Yoonho Cha <0119hoya@naver.com>
2026-03-03 18:52:40 +0900

    fix: security구현으로 인한 userid 필드 제거

--- src/main/java/com/sparta/omin/app/model/cart/dto/CartCreateRequest.java
+++ src/main/java/com/sparta/omin/app/model/cart/dto/CartCreateRequest.java
@@ -3,7 +3,6 @@ package com.sparta.omin.app.model.cart.dto;
 import java.util.UUID;
 
 public record CartCreateRequest(
-        UUID userId,      // TODO: JWT 붙으면 SecurityContext에서 추출로 교체
         UUID storeId,
         UUID productId,
         int quantity
