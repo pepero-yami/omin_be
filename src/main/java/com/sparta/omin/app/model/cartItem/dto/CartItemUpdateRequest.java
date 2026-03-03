477758e0a70577d7075f0307cfe354a63fe7a2c3
Yoonho Cha <0119hoya@naver.com>
2026-03-03 18:51:31 +0900

    fix: storeId 미 사용으로 인한 필드 삭제

--- src/main/java/com/sparta/omin/app/model/cartItem/dto/CartItemUpdateRequest.java
+++ src/main/java/com/sparta/omin/app/model/cartItem/dto/CartItemUpdateRequest.java
@@ -2,5 +2,5 @@ package com.sparta.omin.app.model.cartItem.dto;
 
 import java.util.UUID;
 
-public record CartItemUpdateRequest(UUID storeId, UUID productId, int quantity) {
+public record CartItemUpdateRequest(UUID productId, int quantity) {
 }
