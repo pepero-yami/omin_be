a38af3f8a725d15a5c89198d5d0339be8a82d118
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-03-04 11:41:29 +0900

    chore: Create DTO 별점 필수 valid 추가

--- src/main/java/com/sparta/omin/app/model/review/dto/ReviewCreateRequest.java
+++ src/main/java/com/sparta/omin/app/model/review/dto/ReviewCreateRequest.java
@@ -2,11 +2,14 @@ package com.sparta.omin.app.model.review.dto;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 
 import java.util.UUID;
 
 public record ReviewCreateRequest(
+        @NotNull
         UUID orderId,
+        @NotNull
         @Min(0)
         @Max(5)
         double rating,
