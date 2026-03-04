6fc0f4b62333f504283125d4609e98db7101712c
ji-circle <sp202401jw@gmail.com>
2026-03-05 02:49:59 +0900

    audit: JPA Auditing에 createdBy/updatedBy 자동 주입 및 로그인 사용자 UUID 제공

--- src/main/java/com/sparta/omin/common/entity/BaseTimeEntity.java
+++ src/main/java/com/sparta/omin/common/entity/BaseTimeEntity.java
@@ -3,13 +3,14 @@ package com.sparta.omin.common.entity;
 import jakarta.persistence.Column;
 import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
-import lombok.Getter;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
 import java.time.LocalDateTime;
 import java.util.UUID;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter
 @MappedSuperclass
@@ -24,9 +25,11 @@ public abstract class BaseTimeEntity {
     @Column(name = "updated_at", nullable = false)
     protected LocalDateTime updatedAt;
 
+    @CreatedBy
     @Column(name = "created_by", nullable = false, updatable = false)
     protected UUID createdBy;
 
+    @LastModifiedBy
     @Column(name = "updated_by", nullable = false)
     protected UUID updatedBy;
 
