e33d8740ef1e4c78583241956c00f692e4837703
Yoonho Cha <0119hoya@naver.com>
2026-03-05 01:01:14 +0900

    feat: 조회 메서드 추가

--- src/main/java/com/sparta/omin/app/model/payment/repos/PaymentRepository.java
+++ src/main/java/com/sparta/omin/app/model/payment/repos/PaymentRepository.java
@@ -1,6 +1,8 @@
 package com.sparta.omin.app.model.payment.repos;
 
 import com.sparta.omin.app.model.payment.entity.Payment;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
@@ -8,4 +10,6 @@ import java.util.UUID;
 
 public interface PaymentRepository extends JpaRepository<Payment, UUID> {
     Optional<Payment> findByOrderIdAndUserIdAndIsDeletedFalse(UUID orderId, UUID userId);
+    Page<Payment> findByUserIdAndIsDeletedFalse(UUID userId, Pageable pageable);
+    Optional<Payment> findByIdAndIsDeletedFalse(UUID paymentId);
 }
