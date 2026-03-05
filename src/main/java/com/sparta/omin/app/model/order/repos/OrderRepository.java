f0f455489819749e2569d8332985c17c4555669c
Yoonho Cha <0119hoya@naver.com>
2026-03-05 17:02:07 +0900

    feat: order Repository 작성

--- src/main/java/com/sparta/omin/app/model/order/repos/OrderRepository.java
+++ src/main/java/com/sparta/omin/app/model/order/repos/OrderRepository.java
@@ -1,11 +1,8 @@
 package com.sparta.omin.app.model.order.repos;
 
 import com.sparta.omin.app.model.order.entity.Order;
-import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
-import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
