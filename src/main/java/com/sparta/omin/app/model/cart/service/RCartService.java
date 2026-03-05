4bfa29408d52e22fc075babb1d31ed7f96941675
sunsik17 <17sunsik@gmail.com>
2026-03-05 14:26:11 +0900

    feat: cart add product flow

---
+++ src/main/java/com/sparta/omin/app/model/cart/service/RCartService.java
@@ -0,0 +1,10 @@
+package com.sparta.omin.app.model.cart.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class RCartService {
+
+}
