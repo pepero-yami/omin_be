b7e0154ef857ec4b7b79630cdf70e50d5ac7e81d
sunsik17 <17sunsik@gmail.com>
2026-03-07 00:27:09 +0900

    feat: implement Redis cart entity and service

--- src/main/java/com/sparta/omin/app/model/cart/entity/RCart.java
+++ src/main/java/com/sparta/omin/app/model/cart/entity/RCart.java
@@ -6,11 +6,13 @@ import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.redis.core.RedisHash;
 
-@Data
+@Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @RedisHash("Cart")
 public class RCart {
@@ -18,25 +20,36 @@ public class RCart {
 	@Id
 	private UUID customerId;
 	private UUID storeId;
-	private int totalPrice;
+	private Double totalPrice;
+	@Builder.Default
 	private List<Product> products = new ArrayList<>();
-	private List<String> messages = new ArrayList<>();
 
 	public RCart(UUID id) {
 		this.customerId = id;
 	}
 
-	public void addMessage(String message) {
-		this.messages.add(message);
+	public void calculateTotalPrice() {
+		this.totalPrice = products.stream()
+			.mapToDouble(Product::getTotalPrice)
+			.sum();
 	}
 
-	@Data
+	@Getter
 	@NoArgsConstructor
 	@AllArgsConstructor
 	@Builder
 	public static class Product {
-		private UUID productId;
+
+		private UUID id;
 		private String name;
+		private Double price;
+		private Double totalPrice;
 		private int quantity;
+
+		public void add(int quantity) {
+			this.quantity += quantity;
+			this.totalPrice = this.price * this.quantity;
+		}
 	}
 }
+
