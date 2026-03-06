9aa27ad1d2030c7ec733bb02c94830aa856f1278
sunsik17 <17sunsik@gmail.com>
2026-03-07 00:29:44 +0900

    refactor: rename Service package to service

---
+++ src/main/java/com/sparta/omin/app/model/product/service/ProductReadService.java
@@ -0,0 +1,23 @@
+package com.sparta.omin.app.model.product.service;
+
+import com.sparta.omin.app.model.product.entity.Product;
+import com.sparta.omin.app.model.product.repos.ProductRepository;
+import com.sparta.omin.common.error.ApiException;
+import com.sparta.omin.common.error.constants.ErrorCode;
+import java.util.UUID;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProductReadService {
+
+	private final ProductRepository productRepository;
+
+	public Product getProductInStore(UUID productId, UUID storeId) {
+		return productRepository.findByIdAndStoreId(productId, storeId).orElseThrow(
+			() -> new ApiException(ErrorCode.PRODUCT_NOT_FOUND)
+		);
+	}
+
+}
