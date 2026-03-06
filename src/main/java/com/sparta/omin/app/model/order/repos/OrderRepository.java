f4e5ea61377da5e67ad1f3c5fc0eba436bd7e1b1
Yoonho Cha <0119hoya@naver.com>
2026-03-06 18:56:22 +0900

    feat: 가게에 들어온 주문 요청목록 기능 구현

--- src/main/java/com/sparta/omin/app/model/order/repos/OrderRepository.java
+++ src/main/java/com/sparta/omin/app/model/order/repos/OrderRepository.java
@@ -1,16 +1,17 @@
 package com.sparta.omin.app.model.order.repos;
 
-import com.sparta.omin.app.model.order.dto.OrderResponse;
 import com.sparta.omin.app.model.order.entity.Order;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-    Page<Order> findByIdAndUserIdAndIsDeletedFalse(UUID orderId, UUID userId, Pageable pageable);
-    Page<OrderResponse> findByUserIdAndIsDeletedFalse(UUID userId, Pageable pageable);
+    Slice<Order> findByIdAndUserIdAndIsDeletedFalse(UUID orderId, UUID userId, Pageable pageable);
+    Slice<Order> findByUserIdAndIsDeletedFalse(UUID userId, Pageable pageable);
     Optional<Order> findByIdAndIsDeletedFalse(UUID orderId);
+
+    Slice<Order> findByStoreIdAndIsDeletedFalseOrderByCreatedAtDesc(UUID storeId, Pageable pageable);
 }
