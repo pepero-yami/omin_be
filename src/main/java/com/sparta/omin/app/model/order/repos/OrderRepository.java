0a1bb12e0c023102591f05b2d664ab2f6d7647dc
Yoonho Cha <0119hoya@naver.com>
2026-03-06 11:34:44 +0900

    feat: 주문 repositroy 초안 작성

--- src/main/java/com/sparta/omin/app/model/order/repos/OrderRepository.java
+++ src/main/java/com/sparta/omin/app/model/order/repos/OrderRepository.java
@@ -1,9 +1,16 @@
 package com.sparta.omin.app.model.order.repos;
 
+import com.sparta.omin.app.model.order.dto.OrderResponse;
 import com.sparta.omin.app.model.order.entity.Order;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
+    Page<Order> findByIdAndUserIdAndIsDeletedFalse(UUID orderId, UUID userId, Pageable pageable);
+    Page<OrderResponse> findByUserIdAndIsDeletedFalse(UUID userId, Pageable pageable);
+    Optional<Order> findByIdAndIsDeletedFalse(UUID orderId);
 }
