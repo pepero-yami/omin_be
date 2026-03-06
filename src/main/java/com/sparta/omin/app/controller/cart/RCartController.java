9f489e6385fae43b7b53606d7d0ff2ec2e30e307
sunsik17 <17sunsik@gmail.com>
2026-03-07 00:28:36 +0900

    feat: add cart APIs and related error codes

--- src/main/java/com/sparta/omin/app/controller/cart/RCartController.java
+++ src/main/java/com/sparta/omin/app/controller/cart/RCartController.java
@@ -3,26 +3,44 @@ package com.sparta.omin.app.controller.cart;
 import com.sparta.omin.app.application.CartApplication;
 import com.sparta.omin.app.model.cart.dto.CartAddProductRequest;
 import com.sparta.omin.app.model.cart.entity.RCart;
+import com.sparta.omin.app.model.cart.service.RCartService;
 import com.sparta.omin.app.model.user.entity.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/cart")
+@RequestMapping("/api/v1/carts")
 public class RCartController {
 
 	private final CartApplication cartApplication;
+	private final RCartService cartService;
 
 	@PostMapping
-	public ResponseEntity<RCart> addCart(
+	public ResponseEntity<RCart> addCartProduct(
 		@AuthenticationPrincipal User user,
 		@RequestBody CartAddProductRequest request) {
-		return ResponseEntity.ok(cartApplication.addCart(user.getId(), request));
+		return ResponseEntity.status(HttpStatus.CREATED)
+			.body(cartApplication.addCart(user.getId(), request));
+	}
+
+	@GetMapping
+	public ResponseEntity<RCart> getCartInfo(@AuthenticationPrincipal User user) {
+		return ResponseEntity.ok().body(cartService.getCartInfo(user.getId()));
+	}
+
+	@PutMapping
+	public ResponseEntity<RCart> refreshCart(@AuthenticationPrincipal User user) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(
+			cartService.refresh(user.getId())
+		);
 	}
 }
