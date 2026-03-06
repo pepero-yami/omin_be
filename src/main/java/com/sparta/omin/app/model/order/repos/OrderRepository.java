79b49dca64eaca25000d200466546e2c2ad1560d
Yoonho Cha <0119hoya@naver.com>
2026-03-07 00:11:30 +0900

    refactor: n+1 이슈 해소

--- src/main/java/com/sparta/omin/app/model/order/repos/OrderRepository.java
+++ src/main/java/com/sparta/omin/app/model/order/repos/OrderRepository.java
@@ -4,14 +4,17 @@ import com.sparta.omin.app.model.order.entity.Order;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface OrderRepository extends JpaRepository<Order, UUID> {
-    Slice<Order> findByIdAndUserIdAndIsDeletedFalse(UUID orderId, UUID userId, Pageable pageable);
     Slice<Order> findByUserIdAndIsDeletedFalse(UUID userId, Pageable pageable);
+    @Query("SELECT o FROM Order o " +
+            "JOIN FETCH o.store " +
+            "LEFT JOIN FETCH o.orderItems " +
+            "WHERE o.id = :orderId AND o.isDeleted = false")
     Optional<Order> findByIdAndIsDeletedFalse(UUID orderId);
-
     Slice<Order> findByStoreIdAndIsDeletedFalseOrderByCreatedAtDesc(UUID storeId, Pageable pageable);
 }
