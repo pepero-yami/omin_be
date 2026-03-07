931b69314e303f4116cb6e00143807ed76c684a6
SeongWon Jeong <newon.jsw@gmail.com>
2026-03-07 12:04:24 +0900

    refactor: delete unused import code

--- src/main/java/com/sparta/omin/app/model/product/entity/Product.java
+++ src/main/java/com/sparta/omin/app/model/product/entity/Product.java
@@ -7,7 +7,6 @@ import jakarta.persistence.*;
 
 import java.util.UUID;
 import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
