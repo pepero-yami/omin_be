e47718978c4be3afeaade7b8e88084dfcfbaf6d3
SeongWon Jeong <newon.jsw@gmail.com>
2026-03-07 12:06:36 +0900

    feat: implements retrieving product list

--- src/main/java/com/sparta/omin/app/model/product/repos/ProductRepository.java
+++ src/main/java/com/sparta/omin/app/model/product/repos/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.sparta.omin.app.model.product.repos;
 
 import com.sparta.omin.app.model.product.entity.Product;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +9,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface ProductRepository extends JpaRepository<Product, UUID> {
 
     Optional<Product> findByIdAndIsDeletedFalse(UUID productId);
+
+    List<Product> findByStoreIdAndIsDeletedFalse(UUID storeId);
 }
