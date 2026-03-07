d52819ef35b1bd115392ce02931976e9f3bf9b41
SeongWon Jeong <newon.jsw@gmail.com>
2026-03-07 20:42:25 +0900

    refactor: use OminBusinessException instead of CommonException

--- src/main/java/com/sparta/omin/app/model/store/service/StoreReadService.java
+++ src/main/java/com/sparta/omin/app/model/store/service/StoreReadService.java
@@ -2,9 +2,8 @@ package com.sparta.omin.app.model.store.service;
 
 import com.sparta.omin.app.model.store.entity.Store;
 import com.sparta.omin.app.model.store.repos.StoreRepository;
-import com.sparta.omin.app.model.user.service.UserReadService;
+import com.sparta.omin.common.error.OminBusinessException;
 import com.sparta.omin.common.error.constants.ErrorCode;
-import com.sparta.omin.common.error.exceptions.CommonException;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,7 +18,7 @@ public class StoreReadService {
      * 자신이 소유한 Store인지 검증합니다.
      */
     public Boolean isOwnedStore(UUID storeId, UUID userId) {
-        Store store = storeRepository.findById(storeId).orElseThrow(() -> new CommonException(ErrorCode.STORE_NOT_FOUND));
+        Store store = storeRepository.findById(storeId).orElseThrow(() -> new OminBusinessException(ErrorCode.STORE_NOT_FOUND));
         return userId.equals(store.getOwnerId());
     }
 
