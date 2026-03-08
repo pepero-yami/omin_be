c5140afa115f1f32295985867f9f3ed0175a2869
sunsik17 <17sunsik@gmail.com>
2026-03-09 01:09:22 +0900

    feat: add product deletion flow in cart
    - Introduced `CartProductDeleteRequest` DTO for product deletion.
    - Added `deleteProductInCart` method in `CartApplication`.
    - Created endpoint to handle product deletion in `RCartController`.

--- src/main/java/com/sparta/omin/app/application/CartApplication.java
+++ src/main/java/com/sparta/omin/app/application/CartApplication.java
@@ -1,11 +1,12 @@
 package com.sparta.omin.app.application;
 
 import com.sparta.omin.app.model.cart.dto.CartAddProductRequest;
+import com.sparta.omin.app.model.cart.dto.request.CartProductDeleteRequest;
 import com.sparta.omin.app.model.cart.entity.RCart;
 import com.sparta.omin.app.model.cart.service.RCartService;
-import com.sparta.omin.app.model.product.service.ProductReadService;
 import com.sparta.omin.app.model.product.code.ProductStatus;
 import com.sparta.omin.app.model.product.entity.Product;
+import com.sparta.omin.app.model.product.service.ProductReadService;
 import com.sparta.omin.common.error.OminBusinessException;
 import com.sparta.omin.common.error.constants.ErrorCode;
 import java.util.UUID;
@@ -17,6 +18,7 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @RequiredArgsConstructor
 public class CartApplication {
+
 	private final RCartService cartService;
 	private final ProductReadService productReadService;
 
@@ -38,6 +40,16 @@ public class CartApplication {
 		return cartService.save(cart);
 	}
 
+	public RCart deleteProductInCart(UUID customerId, CartProductDeleteRequest request) {
+		RCart cart = cartService.getCartInCustomer(customerId, request.storeId());
+		cart.getProducts().removeIf(p -> request.productIds().contains(p.getId()));
+		if (cart.getProducts().isEmpty()) {
+			return cartService.refresh(customerId);
+		}
+		cart.calculateTotalPrice();
+		return cartService.save(cart);
+	}
+
 	private void validateProductStatus(Product product) {
 		if (product.getStatus() != ProductStatus.ON_SALE) {
 			throw new OminBusinessException(ErrorCode.PRODUCT_IS_NOT_AVAILABLE_FOR_SALE);
@@ -48,15 +60,5 @@ public class CartApplication {
 		if (!cart.getStoreId().equals(requestStoreId)) {
 			throw new OminBusinessException(ErrorCode.CART_STORE_CONFLICT);
 		}
-	}
-
-	private void addNewProduct(RCart cart, Product product, int quantity) {
-		cart.getProducts().add(RCart.Product.builder()
-			.id(product.getId())
-			.name(product.getName())
-			.price(product.getPrice())
-			.quantity(quantity)
-			.totalPrice(product.getPrice() * quantity)
-			.build());
 	}
 }
