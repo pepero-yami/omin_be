78d2a8aab16f9462d3b5dc86f5377286c54f9930
SeongWon Jeong <newon.jsw@gmail.com>
2026-03-09 23:24:56 +0900

    chore: delete unused method

--- src/main/java/com/sparta/omin/app/model/product/service/ProductReadService.java
+++ src/main/java/com/sparta/omin/app/model/product/service/ProductReadService.java
@@ -35,17 +35,6 @@ public class ProductReadService {
     }
 
     /**
-     * 메뉴의 요약 정보를 반환합니다.<br>
-     * 설명을 포함하지 않는 {@link ProductSummaryResult} 를 반환합니다.<br>
-     * 설명이 필요하지 않은 cart 또는 order에서 사용하세요.
-     */
-    public ProductSummaryResult getProductSummary(UUID productId) {
-        return productRepository.findByIdAndIsDeletedFalse(productId)
-            .map(ProductSummaryResult::from)
-            .orElseThrow(() -> new OminBusinessException(ErrorCode.PRODUCT_NOT_FOUND));
-    }
-
-    /**
      * 매장에 등록되어있는 메뉴들의 목록을 반환합니다.<br>
      * 품절된 메뉴의 경우, UI에서 "품절되었습니다"메세지가 뜨는 것을 가정하여, 포함하여 반환합니다.
      * @param storeId
