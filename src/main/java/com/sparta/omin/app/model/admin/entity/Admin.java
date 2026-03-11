5613b2c8fedf4c39d0f0143178162b3ae8a73e7a
sunsik17 <17sunsik@gmail.com>
2026-03-11 12:27:03 +0900

    refactor: remove unused imports from AdminAuthController and Admin entity

--- src/main/java/com/sparta/omin/app/model/admin/entity/Admin.java
+++ src/main/java/com/sparta/omin/app/model/admin/entity/Admin.java
@@ -11,7 +11,6 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import java.util.Collection;
-import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 import lombok.AccessLevel;
