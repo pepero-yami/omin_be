f074dc18e71f3b6bd2c079902924bb7af1d3e7b6
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-03-11 13:33:41 +0900

    chore: Length -> Size

--- src/main/java/com/sparta/omin/app/model/review/dto/ReviewCreateRequest.java
+++ src/main/java/com/sparta/omin/app/model/review/dto/ReviewCreateRequest.java
@@ -3,7 +3,7 @@ package com.sparta.omin.app.model.review.dto;
 import jakarta.validation.constraints.DecimalMax;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
-import org.hibernate.validator.constraints.Length;
+import jakarta.validation.constraints.Size;
 
 import java.util.UUID;
 
@@ -14,7 +14,7 @@ public record ReviewCreateRequest(
         @DecimalMin(value = "1.0")
         @DecimalMax(value = "5.0")
         Double rating,
-        @Length(max = 300, message = "코멘트는 300자를 넘길 수 없습니다.")
+        @Size(max = 300, message = "코멘트는 300자를 넘길 수 없습니다.")
         String comment
 ) {
 }
