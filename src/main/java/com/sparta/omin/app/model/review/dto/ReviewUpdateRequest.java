14e253cf17ff96e3e6b692b24fde960d78e8dc79
SEOUNJU PARK <sj.ella1030@gmail.com>
2026-03-11 13:03:25 +0900

    fix : dto valid

--- src/main/java/com/sparta/omin/app/model/review/dto/ReviewUpdateRequest.java
+++ src/main/java/com/sparta/omin/app/model/review/dto/ReviewUpdateRequest.java
@@ -1,15 +1,18 @@
 package com.sparta.omin.app.model.review.dto;
 
 import com.sparta.omin.app.model.review.entity.ReviewImage;
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Size;
 
 import java.util.List;
 
 public record ReviewUpdateRequest(
-        @Min(1)
-        @Max(5)
+        @DecimalMin(value = "1.0")
+        @DecimalMax(value = "5.0")
         Double rating,
+
+        @Size(max = 300, message = "코멘트는 300자를 넘길 수 없습니다.")
         String comment,
         List<ReviewImage> updateImages,
         List<String> deleteImages
